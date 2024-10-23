172cde8b237d1003108099828ffdc1dc717e8a36
Fink7171 <79508075+Fink7171@users.noreply.github.com>
2024-10-23 13:11:49 +0200

    Moved over a bunch of Catalog API points

---
+++ eShopOnWebStrangler/eShopOnWebCatalog/Specifications/CatalogItemNameSpecification.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+using eShopOnWebCatalog.Entities;
+
+
+namespace Microsoft.eShopWeb.ApplicationCore.Specifications;
+
+public class CatalogItemNameSpecification : Specification<CatalogItem>
+{
+    public CatalogItemNameSpecification(string catalogItemName)
+    {
+        Query.Where(item => catalogItemName == item.Name);
+    }
+}
